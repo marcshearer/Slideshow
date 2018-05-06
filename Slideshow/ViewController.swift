5cbfedea1994a22b916e64533de574afea9f575c
Marc Shearer <marc.shearer@tesco.net>
2018-05-06 12:49:16 +0100

    Initial Commit

---
+++ Slideshow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slideshow
+//
+//  Created by Marc Shearer on 06/05/2018.
+//  Copyright © 2018 Marc Shearer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
